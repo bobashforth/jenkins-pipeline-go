ac8a52b5895a1f2fecaa2e13a39e2e3d47e6d7fa
Allan Selvan <allantony2008@gmail.com>
2018-01-21 21:05:12 +0100

    testing shared libraries

--- vars/uploadToNexus.groovy
+++ vars/uploadToNexus.groovy
@@ -3,8 +3,8 @@ def call(final String repositoryId, final String groupId, final String artifactI
          final String version, final String packaging, final String filePath) {
 
     nexusPublisher nexusInstanceId: 'privatesquare-nexus',
-            nexusRepositoryId: "repositoryId",
+            nexusRepositoryId: repositoryId,
             packages: [[$class: 'MavenPackage',
-                        mavenAssetList: [[classifier: '', extension: '', filePath: "./filePath"]],
-                        mavenCoordinate: [artifactId: "artifactId", groupId: "groupId", packaging: "packaging", version: "version"]]]
+                        mavenAssetList: [[classifier: '', extension: '', filePath: filePath]],
+                        mavenCoordinate: [artifactId: artifactId, groupId: groupId, packaging: packaging, version: version]]]
 }
